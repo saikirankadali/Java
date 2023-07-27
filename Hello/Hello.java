ba5623a9b6f75166c92bed5f385a8580c7d5940c
saikiran <saikiranxkadali@gmail.com>
2023-07-27 16:42:37 +0530

    Hello

--- Hello/Hello.java
+++ Hello/Hello.java
@@ -1,6 +1,8 @@
+package Hello;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println(" Hello");
     }
 
 }
--- world/World.java
+++
@@ -1,8 +0,0 @@
-package world;
-
-public class World {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-
-}
