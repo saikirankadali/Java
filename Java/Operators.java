33c3e5cb9e82e8a4877fec9773bd8e6148fb3838
saikiran <saikiranxkadali@gmail.com>
2023-07-27 22:23:08 +0530

    Operators

---
+++ Java/Operators.java
@@ -0,0 +1,16 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 20;
+
+        int sum = a + b;
+        // System.out.println(sum);
+
+        int mul = a * b;
+        // System.out.println(mul);
+
+        int sub = a - b;
+        System.out.println(sub);
+
+    }
+}
